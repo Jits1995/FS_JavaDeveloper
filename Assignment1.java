c6089f0425b2f603c1c7d4a09aa2ad6bc4a54c28
Jitendra Makvana <Jitumakvana1995@gmail.com>
2022-02-20 15:52:42 +0530

    Removed Package Declaration from Files

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,3 @@
-package mypackage;
-
 public class Assignment1 {
 
 	public static void main(String[] args) {
--- Assignment2.java
+++ Assignment2.java
@@ -1,5 +1,3 @@
-package mypackage;
-
 public class Assignment2{
 
 	public static void main(String[] args) {
