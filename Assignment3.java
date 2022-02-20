3c54a1c417bd67d01ff9dd5787eb7ae16ef92b19
Jitendra Makvana <Jitumakvana1995@gmail.com>
2022-02-20 15:52:02 +0530

    FS Java Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,17 @@
+public class Assignment3{
+
+	public static void main(String[] args) {
+
+		int num = 4;
+
+		if(num%2==0){
+			System.out.println(num + " is Even number");
+
+		}else{
+			System.out.println(num + " is Odd number");
+
+		}
+
+	}
+
+}
